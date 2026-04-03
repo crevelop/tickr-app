f9cb636a2ed6acb20c13fc36a5ef79e78be3c8ec
Carlos <crevelop@gmail.com>
2026-04-03 15:20:42 -0500

    chore: support local sdk for faster development

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
+const path = require("path");
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  transpilePackages: ["@oddmaki-protocol/sdk"],
+  // TODO: Remove turbopack.root when using the stable @oddmaki-protocol/sdk from npm
+  turbopack: {
+    root: path.resolve(__dirname, ".."),
+  },
+};
 
 module.exports = nextConfig;
